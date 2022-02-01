f64fb972825b9641c3d543af1912b87c9b2fc6b2
Ankit Sharma <sharmankit9593@gmail.com>
2022-02-01 23:34:59 +0530

    refactored code

--- crawler.js
+++ crawler.js
@@ -1,5 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const xl = require('excel4node');
+const fs = require('fs');
 
 request('https://stackoverflow.com/questions', (err, res, html) => {
     if(err) {
@@ -14,17 +16,52 @@ request('https://stackoverflow.com/questions', (err, res, html) => {
         let voteArr = vote('.vote-count-post>strong');
         let ans = cheerio.load(html);
         let answerArr = ans('.status>strong');
-        handleHtml(question, questionArr, vote, voteArr, ans, answerArr, 0);
+        const ques = [], votes = [], answer = [];
+        handleHtml(question, questionArr, vote, voteArr, ans, answerArr, 0, ques, votes, answer);
+
+        const json =
+        {
+            "ques": ques,
+            "votes": votes,
+            "answers": answer
+        };
+         // let newWb = xlsx.utils.book_new();
+        // let newWs = xlsx.utils.json_to_sheet(obj);
+        // // console.log(JSON.stringify(json));
+        // xlsx.utils.book_append_sheet(newWb, newWs, 'sheet-1');
+        // xlsx.writeFile(newWb, 'data.xlsx');
+        const wb = new xl.Workbook();
+        const ws = wb.addWorksheet('sheet');
+        const headingColNames = [
+            'Question',
+            'Votes',
+            'Answers'
+        ]
+        let headingColIndex  = 1;
+        headingColNames.forEach(heading => {
+            ws.cell(1, headingColIndex++).string(heading);
+        });
+
+        let rowIndex = 2;
+        let colIndex = 1;   
+        json['ques'].forEach(record => {
+            Object.keys(record).forEach(columnName => {
+                ws.cell(rowIndex++, colIndex++).string(record[columnName]);
+            });
+        });
+        wb.write('data.xlsx');
+
     }
 });
 
-function handleHtml(question, questionArr, vote, voteArr, ans, answerArr, i) {
+function handleHtml(question, questionArr, vote, voteArr, ans, answerArr, i, ques, votes, answer) {
     if(i >= questionArr.length || i >= voteArr.length || i >= answerArr.length) return;
     
-    let ques = question(questionArr[i]).text();
-    let votes = vote(voteArr[i]).text();
-    let answer = ans(answerArr[i]).text();
+    ques.push(question(questionArr[i]).text());
+    votes.push(vote(voteArr[i]).text());
+    answer.push(ans(answerArr[i]).text());
     
-    console.log('Question: ' +  ques + ', Votes: ' + votes +  ', Answers: ' + answer);
-    handleHtml(question, questionArr, vote, voteArr, ans, answerArr, i+1);
+    // console.log('Question: ' +  ques + ', Votes: ' + votes +  ', Answers: ' + answer);
+    
+    handleHtml(question, questionArr, vote, voteArr, ans, answerArr, i+1, ques, votes, answer);
 }
